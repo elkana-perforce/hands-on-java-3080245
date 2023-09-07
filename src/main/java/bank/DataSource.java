169a26e42c42eb242e54580827b0e45d2d7c61ac
elkana-perforce <81908984+elkana-perforce@users.noreply.github.com>
2023-09-07 16:20:35 +0000

    Add getCustomer method

---
+++ src/main/java/bank/DataSource.java
@@ -0,0 +1,54 @@
+package bank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DataSource {
+  public static Connection connect() {
+    String db_file = "jdbc:sqlite:resources/bank.db";
+
+    Connection connection = null;
+
+    try {
+      connection = DriverManager.getConnection(db_file);
+      System.out.println("wer'e connected");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return connection;
+
+  }
+
+
+  public static Customer getCustomer(String username) {
+    String sql = "SELECT * FROM customers where username=?";
+    Customer customer = null;
+
+    try (Connection connection = connect(); 
+    PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, username);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        customer = new Customer(
+          resultSet.getInt("id"),
+          resultSet.getString("name"),
+          resultSet.getString("username"),
+          resultSet.getString("password"),
+          resultSet.getInt("account_id"));
+      } catch (Exception e) {
+        // TODO: handle exception
+      }      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return customer;
+  }
+
+  public static void main(String[] args) {
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(customer.getName());
+  }
+}
